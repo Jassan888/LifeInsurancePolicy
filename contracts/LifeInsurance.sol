b1ab28147badeae587069f6b675d94ad4a600ffc
jeffrey <jeffreyassan88@gmail.com>
2020-11-28 14:32:56 -0500

    project file update

--- contracts/LifeInsurance.sol
+++ contracts/LifeInsurance.sol
@@ -1,6 +1,6 @@
-pragma solidity >=0.4.22 <0.8.0;
+pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/math/SafeMath.sol";
 
 contract LifeInsurance{
     using SafeMath for uint;
--- migrations/2-initial_deployment_migration.js
+++ migrations/2-initial_deployment_migration.js
@@ -1,6 +1,5 @@
 const LifeInsurance = artifacts.require('LifeInsurance');
 
 module.exports = function (deployer) {
-  deployer.deploy(LifeInsurance);
+  deployer.deploy(LifeiInsurance);
 };
-
